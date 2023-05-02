eb03e1d6aaec51a7961e02664ae66762122773c9
oscarnovember <ndukwe.peter@roqqu.com>
2023-05-02 13:19:27 +0100

    separate min/max value from min/max length

--- pkg/validate/schema.go
+++ pkg/validate/schema.go
@@ -27,14 +27,68 @@ func NewSchema() *Schema {
 }
 
 /*
-TODO: Implement this function to return a vaidation result depending on the status of the object
+TODO: Implement this function to return a validation result depending on the status of the object
 */
 func (s *Schema) Validate(obj interface{}) ValidationResult {
+	fmt.Println("the validate function is running")
+	result := ValidationResult{}
+	isValid := true
 	for _, field := range s.content.fields {
+		/*
+			TODO: If the field is not required, you still need to validate it to ensure that it has all the right properties
+		*/
+		if !field.isRequired {
+			continue
+		}
+		/*
+			If found == true, it means that the object has a field/property with the given name
+		*/
 		_, found := reflect.TypeOf(obj).FieldByName(field.name)
-		fmt.Println(found)
+		if !found {
+			isValid = false
+			result.Reasons = append(result.Reasons, ValidationReason{
+				Value:   field.name,
+				Message: "the field '" + field.name + "' was not found in the object",
+			})
+			continue
+		}
+		// add a result reason if its value is below the min value
+		// dataTypeOfSchema := s.content.fields[len(s.content.fields)-1].dataType
+		/*
+			You have the name of the field you are looking for. Check the value of the field of the object
+			that has that name
+
+			If the value of the error is not nil, the object you are validating has no field with the current field name
+			If the value of [value] is not nil, the field exists. This goes without saying.
+		*/
+		value, err := getValueOfFieldWithName(obj, s.content.fields[len(s.content.fields)-1].name)
+		if err != nil {
+			isValid = false
+			result.Reasons = append(result.Reasons, ValidationReason{
+				Value:   field.name,
+				Message: "the field '" + field.name + "' was not found in the object",
+			})
+			continue
+		}
+		// fmt.Println("dataTypeOfSchema => " + dataTypeOfSchema + "\nvalue of field with name " + field.name + " => ")
+		fmt.Println(value)
+		fmt.Println("")
+		// If thid doesn't work I might have to convert all numeric types to float64 and store them like that
+		if fieldNameIsNumericType(field.name) {
+			fmt.Println("the value is a numeric type")
+			// if floatValue, err := value.(float64); !err {
+			// 	fmt.Println("the value is not a float type")
+			// 	result.Reasons = append(result.Reasons, ValidationReason{
+			// 		Value:   field.name,
+			// 		Message: "the field ",
+			// 	})
+			// }
+		} else {
+			fmt.Println("the value is not a numeric type")
+		}
 	}
-	return ValidationResult{}
+	result.IsValid = isValid
+	return result
 }
 
 func (s *Schema) AddField(fieldName string, dataType reflect.Type) *Schema {
@@ -66,18 +120,54 @@ func (s *Schema) SetIsRequired(isRequired bool) *Schema {
 	return s
 }
 
-func (s *Schema) SetMin(value int) *Schema {
+func (s *Schema) SetMinValue(value int) *Schema {
 	if len(s.content.fields) == 0 {
 		s.content.fields = append(s.content.fields, schemaField{})
 	}
-	s.content.fields[len(s.content.fields)-1].min = value
+	currentField := &(s.content.fields[len(s.content.fields)-1])
+	if fieldNameIsNumericType((*currentField).name) {
+		(*currentField).min = value
+	}
 	return s
 }
 
-func (s *Schema) SetMax(value int) *Schema {
+func (s *Schema) SetMaxValue(value int) *Schema {
 	if len(s.content.fields) == 0 {
 		s.content.fields = append(s.content.fields, schemaField{})
 	}
-	s.content.fields[len(s.content.fields)-1].max = value
+	currentField := &(s.content.fields[len(s.content.fields)-1])
+	if fieldNameIsNumericType((*currentField).name) {
+		(*currentField).max = value
+	}
+	return s
+}
+
+func (s *Schema) SetMinLength(value int) *Schema {
+	if len(s.content.fields) == 0 {
+		s.content.fields = append(s.content.fields, schemaField{})
+	}
+
+	/*
+		This method will not work if the field you want to give a minimum length is a numeric
+		field. this is because numeric fields don't have length properties
+	*/
+	if !fieldNameIsNumericType(s.content.fields[len(s.content.fields)-1].dataType) {
+		s.content.fields[len(s.content.fields)-1].min = value
+	}
+	return s
+}
+
+func (s *Schema) SetMaxLength(value int) *Schema {
+	if len(s.content.fields) == 0 {
+		s.content.fields = append(s.content.fields, schemaField{})
+	}
+
+	/*
+		This method will not work if the field you want to give a minimum length is a numeric
+		field. this is because numeric fields don't have length properties
+	*/
+	if !fieldNameIsNumericType(s.content.fields[len(s.content.fields)-1].dataType) {
+		s.content.fields[len(s.content.fields)-1].max = value
+	}
 	return s
 }
