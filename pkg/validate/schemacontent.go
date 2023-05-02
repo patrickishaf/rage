42273d8f160c1ec204fe65b41eb22217e2343e54
oscarnovember <ndukwe.peter@roqqu.com>
2023-05-02 13:18:40 +0100

    move validate function to Schema struct

--- pkg/validate/schemacontent.go
+++ pkg/validate/schemacontent.go
@@ -1,20 +1,11 @@
 package validate
 
 import (
-	"fmt"
 	"reflect"
 )
 
 type SchemaContent struct {
 	fields []schemaField
-}
-
-// This will return a ValidationResult eventually. Just chill for now
-func (s *SchemaContent) Validate(object interface{}) (bool, error) {
-	for i, n := range s.fields {
-		fmt.Println(i, n)
-	}
-	return true, nil
 }
 
 // I can make this to return an object that contains a pointer to the field and another pointer
